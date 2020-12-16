c6fdbb680543c1ad853e3252f3bc64b4dabea843
eshel9 <eshel.bam@gmail.com>
2020-12-16 12:07:48 +0200

    gitignore + untested final implementation of magiclist

--- magic_list.py
+++ magic_list.py
@@ -5,4 +5,8 @@ class MagicList(list):
     def __init__(self, cls_type: TypeVar = None):
         self.cls_type = cls_type
 
-    def __
+    def __setitem__(self, key, value):
+        if self.__len__() == key:
+            self.__add__([value])
+
+        super().__setitem__(key, value)
